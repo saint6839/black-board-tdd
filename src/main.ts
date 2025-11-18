c9ca4183fc9bcf986aa7ef7c936fcc0d3e4d4246
sangyeopchae <sy.chae@teamsparta.co>
2025-11-18 13:31:06 +0900

    feat: tsconfig.json 상용 서비스 수준 엄격하도록 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port: number = Number(process.env['PORT']) || 3000;
+  await app.listen(port);
 }
 bootstrap();
